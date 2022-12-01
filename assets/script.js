5c4147c3573165fd1fab429a1b052d552a171a9d
Jonathan Ray <jonathanjudsonray@gmail.com>
2022-12-01 08:58:48 -0500

    fixed all the JS

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
-for(var x = 0; topics < topics.length; x++) {
+for(var x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
-/*
 
+/*
 if (topic === 'HTML') {
  console.log("Let's study HTML!");
 } else if (topic === 'CSS') {
