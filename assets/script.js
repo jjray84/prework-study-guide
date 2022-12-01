d7aade31a5d83394b2cc8be5e34a75e4fdd601bc
Jonathan Ray <jonathanjudsonray@gmail.com>
2022-12-01 08:33:44 -0500

    additional changes to js

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,24 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(var topics = 0; topics < topics.length; x++){
+    console.log(topics[0]);
+}
+/*
+
+if (topic === 'HTML') {
+ console.log("Let's study HTML!");
+} else if (topic === 'CSS') {
+ console.log("Let's study CSS!");
+} else if (topic === 'Git') {
+ console.log("Let's study Git!");
+} else if (topic === 'JavaScript') {
+ console.log("Let's study JavaScript!");
+} else {
+ console.log('Please try again!');
+}
+
 var shapes = ["triangle", "square", "pentagon", "circle"];
-console.log(shapes[0]);
+var shapes = ["triangle", "square", "pentagon", "circle"];
+for(var x = 0; x < shapes.length; x++) {
+ console.log(shapes[x]);
+}
+*/
